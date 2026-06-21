e52a5db28f7023fcded6585604fa34f2fcfe3d6f
aaronw3355 <avisualofficial@gmail.com>
2026-06-21 15:38:08 -0400

    Update Inventory.h

--- Inventory.h
+++ Inventory.h
@@ -1,1 +1,22 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
 
+#include <vector>
+#include "Product.h"
+
+class Inventory {
+
+private:
+    vector<Product> products;
+
+public:
+    void addProduct(Product product);
+
+    void removeProduct(int id);
+
+    void displayProducts();
+
+    vector<Product>& getProducts();
+};
+
+#endif
