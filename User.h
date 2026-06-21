494406f6a0f2c7f5f495caa2dfe755b18ac6bc1a
aaronw3355 <avisualofficial@gmail.com>
2026-06-21 15:22:10 -0400

    Update User.h

--- User.h
+++ User.h
@@ -1,1 +1,22 @@
+class User{
 
+protected:
+
+string name;
+string email;
+string address;
+
+ShoppingCart cart;
+
+public:
+
+User(
+string n,
+string e,
+string a);
+
+virtual double getDiscount()=0;
+
+ShoppingCart& getCart();
+
+};
