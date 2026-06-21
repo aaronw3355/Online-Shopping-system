b4c7fdb09dbdbc58797ab42b929e9d3d422b0746
aaronw3355 <avisualofficial@gmail.com>
2026-06-21 15:20:30 -0400

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,1 +1,14 @@
+#include "ShoppingCart.h"
 
+void ShoppingCart::addProduct(
+int productID,
+int quantity)
+{
+items[productID]+=quantity;
+}
+
+void ShoppingCart::removeProduct(
+int productID)
+{
+items.erase(productID);
+}
