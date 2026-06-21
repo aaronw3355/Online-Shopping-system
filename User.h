08d9604fb44489e6a944e6b9d7976c4d5f96c4a0
aaronw3355 <avisualofficial@gmail.com>
2026-06-21 15:23:47 -0400

    Update User.h

--- User.h
+++ User.h
@@ -20,3 +20,36 @@ virtual double getDiscount()=0;
 ShoppingCart& getCart();
 
 };
+
+class RegularUser:
+public User{
+
+public:
+
+RegularUser(
+string n,
+string e,
+string a)
+:
+User(n,e,a)
+{}
+
+double getDiscount(){
+
+return 0;
+
+}
+
+};
+
+double getDiscount(){
+
+return .10;
+
+}
+
+double getDiscount(){
+
+return .20;
+
+}
