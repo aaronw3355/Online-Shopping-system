602c8aa481831bbe3fa8538778d78f78f1fb01f2
aaronw3355 <avisualofficial@gmail.com>
2026-06-21 13:04:31 -0400

    Update Product.h

--- Product.h
+++ Product.h
@@ -1,1 +1,38 @@
+#ifndef PRODUCT_H
+#define PRODUCT_H
 
+#include <iostream>
+using namespace std;
+
+class Product {
+
+private:
+    int id;
+    string name;
+    string description;
+    double price;
+    int stock;
+
+public:
+
+    Product(
+        int id,
+        string name,
+        string description,
+        double price,
+        int stock
+    );
+
+    int getID();
+
+    string getName();
+
+    double getPrice();
+
+    int getStock();
+
+    void updateStock(int quantity);
+
+};
+
+#endif
