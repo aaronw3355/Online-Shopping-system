c8ced8c5960c0d4d229840f7cb3638c052adda47
aaronw3355 <avisualofficial@gmail.com>
2026-06-21 14:10:38 -0400

    Update ShoppingCart.h

--- ShoppingCart.h
+++ ShoppingCart.h
@@ -1,1 +1,26 @@
+#ifndef SHOPPINGCART_H
+#define SHOPPINGCART_H
 
+#include <map>
+
+class ShoppingCart{
+
+private:
+
+map<int,int> items;
+
+public:
+
+void addProduct(
+int productID,
+int quantity);
+
+void removeProduct(
+int productID);
+
+double calculateTotal(
+vector<Product>& products);
+
+};
+
+#endif
