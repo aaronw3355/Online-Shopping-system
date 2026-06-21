b24c71ba419d2dd2ec82d6fe588b44eb4a4ffdec
aaronw3355 <avisualofficial@gmail.com>
2026-06-21 13:41:37 -0400

    Update Product.cpp

--- Product.cpp
+++ Product.cpp
@@ -1,1 +1,35 @@
+#include "Product.h"
 
+Product::Product(
+    int id,
+    string name,
+    string description,
+    double price,
+    int stock)
+{
+    this->id=id;
+    this->name=name;
+    this->description=description;
+    this->price=price;
+    this->stock=stock;
+}
+
+int Product::getID(){
+    return id;
+}
+
+string Product::getName(){
+    return name;
+}
+
+double Product::getPrice(){
+    return price;
+}
+
+int Product::getStock(){
+    return stock;
+}
+
+void Product::updateStock(int quantity){
+    stock += quantity;
+}
